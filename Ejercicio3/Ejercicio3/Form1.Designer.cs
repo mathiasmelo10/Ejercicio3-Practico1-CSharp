94efe1ecd5f4606763d73b9533d3292826ca1791
Mathias Melo <melo.mathias01@gmail.com>
2021-05-13 22:38:58 -0300

    Ejercicio 3 Practico 1 CSharp agregado

---
+++ Ejercicio3/Ejercicio3/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Ejercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.lbNombres = new System.Windows.Forms.ListBox();
+            this.lbApellidos = new System.Windows.Forms.ListBox();
+            this.lbTelefonos = new System.Windows.Forms.ListBox();
+            this.lbDirecciones = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblNombres = new System.Windows.Forms.Label();
+            this.lblApellidos = new System.Windows.Forms.Label();
+            this.lblTelefonos = new System.Windows.Forms.Label();
+            this.lblDirecciones = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblDir = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(86, 39);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(198, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(86, 101);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(198, 20);
+            this.txtTelefono.TabIndex = 6;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(86, 69);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(198, 20);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(86, 131);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(198, 20);
+            this.txtDireccion.TabIndex = 8;
+            // 
+            // lbNombres
+            // 
+            this.lbNombres.FormattingEnabled = true;
+            this.lbNombres.Location = new System.Drawing.Point(302, 39);
+            this.lbNombres.Name = "lbNombres";
+            this.lbNombres.Size = new System.Drawing.Size(120, 199);
+            this.lbNombres.TabIndex = 9;
+            // 
+            // lbApellidos
+            // 
+            this.lbApellidos.FormattingEnabled = true;
+            this.lbApellidos.Location = new System.Drawing.Point(428, 39);
+            this.lbApellidos.Name = "lbApellidos";
+            this.lbApellidos.Size = new System.Drawing.Size(120, 199);
+            this.lbApellidos.TabIndex = 11;
+            // 
+            // lbTelefonos
+            // 
+            this.lbTelefonos.FormattingEnabled = true;
+            this.lbTelefonos.Location = new System.Drawing.Point(554, 39);
+            this.lbTelefonos.Name = "lbTelefonos";
+            this.lbTelefonos.Size = new System.Drawing.Size(120, 199);
+            this.lbTelefonos.TabIndex = 12;
+            // 
+            // lbDirecciones
+            // 
+            this.lbDirecciones.FormattingEnabled = true;
+            this.lbDirecciones.Location = new System.Drawing.Point(680, 39);
+            this.lbDirecciones.Name = "lbDirecciones";
+            this.lbDirecciones.Size = new System.Drawing.Size(120, 199);
+            this.lbDirecciones.TabIndex = 13;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(86, 157);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(198, 31);
+            this.btnAgregar.TabIndex = 14;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(86, 198);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(198, 31);
+            this.btnEliminar.TabIndex = 15;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(677, 23);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(0, 13);
+            this.lblDireccion.TabIndex = 18;
+            // 
+            // lblNombres
+            // 
+            this.lblNombres.AutoSize = true;
+            this.lblNombres.Location = new System.Drawing.Point(299, 20);
+            this.lblNombres.Name = "lblNombres";
+            this.lblNombres.Size = new System.Drawing.Size(49, 13);
+            this.lblNombres.TabIndex = 25;
+            this.lblNombres.Text = "Nombres";
+            // 
+            // lblApellidos
+            // 
+            this.lblApellidos.AutoSize = true;
+            this.lblApellidos.Location = new System.Drawing.Point(428, 22);
+            this.lblApellidos.Name = "lblApellidos";
+            this.lblApellidos.Size = new System.Drawing.Size(49, 13);
+            this.lblApellidos.TabIndex = 26;
+            this.lblApellidos.Text = "Apellidos";
+            // 
+            // lblTelefonos
+            // 
+            this.lblTelefonos.AutoSize = true;
+            this.lblTelefonos.Location = new System.Drawing.Point(554, 20);
+            this.lblTelefonos.Name = "lblTelefonos";
+            this.lblTelefonos.Size = new System.Drawing.Size(54, 13);
+            this.lblTelefonos.TabIndex = 27;
+            this.lblTelefonos.Text = "Teléfonos";
+            // 
+            // lblDirecciones
+            // 
+            this.lblDirecciones.AutoSize = true;
+            this.lblDirecciones.Location = new System.Drawing.Point(680, 20);
+            this.lblDirecciones.Name = "lblDirecciones";
+            this.lblDirecciones.Size = new System.Drawing.Size(63, 13);
+            this.lblDirecciones.TabIndex = 28;
+            this.lblDirecciones.Text = "Direcciones";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(31, 46);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 30;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(29, 76);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 31;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(26, 108);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefono.TabIndex = 32;
+            this.lblTelefono.Text = "Teléfono";
+            // 
+            // lblDir
+            // 
+            this.lblDir.AutoSize = true;
+            this.lblDir.Location = new System.Drawing.Point(29, 138);
+            this.lblDir.Name = "lblDir";
+            this.lblDir.Size = new System.Drawing.Size(52, 13);
+            this.lblDir.TabIndex = 33;
+            this.lblDir.Text = "Dirección";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 278);
+            this.Controls.Add(this.lblDir);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblDirecciones);
+            this.Controls.Add(this.lblTelefonos);
+            this.Controls.Add(this.lblApellidos);
+            this.Controls.Add(this.lblNombres);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lbDirecciones);
+            this.Controls.Add(this.lbTelefonos);
+            this.Controls.Add(this.lbApellidos);
+            this.Controls.Add(this.lbNombres);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Ejercicio3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.ListBox lbNombres;
+        private System.Windows.Forms.ListBox lbApellidos;
+        private System.Windows.Forms.ListBox lbTelefonos;
+        private System.Windows.Forms.ListBox lbDirecciones;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblNombres;
+        private System.Windows.Forms.Label lblApellidos;
+        private System.Windows.Forms.Label lblTelefonos;
+        private System.Windows.Forms.Label lblDirecciones;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblDir;
+    }
+}
+
---
+++ Ejercicio3Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            lbNombres.Items.Add(txtNombre.Text);
+            lbApellidos.Items.Add(txtApellido.Text);
+            lbTelefonos.Items.Add(txtTelefono.Text);
+            lbDirecciones.Items.Add(txtDireccion.Text);
+
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtTelefono.Clear();
+            txtDireccion.Clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int indice = lbNombres.SelectedIndex;
+
+            lbNombres.Items.RemoveAt(indice);
+            lbApellidos.Items.RemoveAt(indice);
+            lbTelefonos.Items.RemoveAt(indice);
+            lbDirecciones.Items.RemoveAt(indice);
+        }
+    }
+}
